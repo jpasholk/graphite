961582a2482a18f5e37af97dbc2683f3fa725ead
jpasholk <josh.pasholk@gmail.com>
2018-12-04 17:16:43 -0800

    Getting closer

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -1,17 +1,17 @@
 document.body.style.backgroundColor = sessionStorage.getItem('bg');
 document.body.style.color = sessionStorage.getItem('cc');
 function theme() {
-     if ( sessionStorage.getItem('bg') === 'rgb(255, 255, 255)') {
-        sessionStorage.setItem('bg', 'rgb(6, 23, 37)');
-        sessionStorage.setItem('cc', '#777');
+     if ( sessionStorage.getItem('bg') === '#bac0ce') {
+        sessionStorage.setItem('bg', '#2f2c30');
+        sessionStorage.setItem('cc', '#bac0ce');
      }
     else if (sessionStorage.getItem('bg') == null || undefined) {
-        sessionStorage.setItem('bg', 'rgb(6, 23, 37)');
-        sessionStorage.setItem('cc', '#777');
+        sessionStorage.setItem('bg', '#2f2c30');
+        sessionStorage.setItem('cc', '#bac0ce');
     }
-    else if( sessionStorage.getItem('bg') === 'rgb(6, 23, 37)') {
-        sessionStorage.setItem('bg', 'rgb(255, 255, 255)');
-        sessionStorage.setItem('cc', '#333');
+    else if( sessionStorage.getItem('bg') === '#2f2c30') {
+        sessionStorage.setItem('bg', '#bac0ce');
+        sessionStorage.setItem('cc', '#2f2c30');
     }
 document.body.style.backgroundColor = sessionStorage.getItem('bg');
 document.body.style.color = sessionStorage.getItem('cc');
